99347e83616f1d1bc58a1150597305a57883b8f7
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-04 00:42:24 +0200

    created caching system that uses mru

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+a caching system
+"""
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    a class that creates a caching system that uses mru
+    """
+
+    def __init__(self):
+        """
+        initializes
+        """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """
+        adds item to cache using mru
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                discarded_key = self.order.pop()
+                del self.cache_data[discarded_key]
+                print("DISCARD: {}".format(discarded_key))
+
+            self.cache_data[key] = item
+            self.order = [k for k in self.order if k != key] + [key]
+
+    def get(self, key):
+        """
+        pulls from cache
+        """
+        if key is not None and key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
