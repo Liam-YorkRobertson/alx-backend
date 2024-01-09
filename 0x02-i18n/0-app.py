cfe3ef6768a566aae42906e4b3fd7a7cbdcc42f9
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-01-09 12:14:31 +0200

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -13,9 +13,7 @@ def index():
     """
     route for the index page
     """
-    return render_template('0-index.html',
-                           title="Welcome to Holberton",
-                           header="Hello world")
+    return render_template('0-index.html',)
 
 
 if __name__ == '__main__':
