454780063ef50393e5e3b390583ef0ae6bed5e2e
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-02-06 20:50:12 +0200

    js script that creates and connects to a redis server

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,13 @@
+// script that creates and connects to a redis server
+
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
--- 0x03-queuing_system_in_js/README.md
+++ 0x03-queuing_system_in_js/README.md
@@ -1,1 +1,1 @@
-In this repository we are going to be working with redis, kue, and javascript to create a queuing system
+In this repository we are going to be working with redis, kue, and javascript to create a queuing system. will install redis in this directory instead of home
