4c2980a5d85f6fcb5d6eaaceeb731eac902b13ec
“Liam-YorkRobertson” <liamyorkdeonrobertson@gmail.com>
2024-02-07 19:38:57 +0200

    store hashed value

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,31 @@
+// copied 2, used hashes to store data
+
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err}`);
+});
+
+function hashed() {
+  client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+  client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+  client.hset('HolbertonSchools', 'New York', 20, redis.print);
+  client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+  client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+  client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+}
+
+function display() {
+  client.hgetall('HolbertonSchools', (_, reply) => {
+    console.log(reply);
+  });
+}
+
+hashed();
+display();
